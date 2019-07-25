6ef347e90302d2c8c18f18c5444e2930886771b0
pradeep143535 <53001023+pradeep143535@users.noreply.github.com>
2019-07-25 14:53:36 +0530

    Update and rename helloworld.java to hellopradeep.java

---
+++ hellopradeep.java
@@ -0,0 +1,6 @@
+public class helloworld {
+   public static void main(String[] args) {
+      // Prints "Hello Pradeep Reddy" in the terminal window.
+      System.out.println("Hello Pradeep Reddy");
+   }
+}
